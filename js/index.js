6e359bae391d7299bab8b2aa222e7045edfbe244
Alexey Bulatov <bulatov.a.v.89@gmail.com>
2023-06-19 13:15:15 +0300

    finished website

---
+++ js/index.js
@@ -0,0 +1,15 @@
+// const headerRef = document.querySelector(".header");
+// const menuRef = document.querySelector(".menu");
+// const btnBurgerOpen = document.querySelector("#btn-burger-open");
+// const btnBurgerClose = document.querySelector("#btn-burger-close");
+
+// window.addEventListener("click", (event) => {
+//   if (event.target.getAttribute("id") === "btn-burger-open") {
+//     headerRef.classList.add("none");
+//     menuRef.classList.remove("none");
+//   }
+//   if (event.target.getAttribute("id") === "btn-burger-close") {
+//     headerRef.classList.remove("none");
+//     menuRef.classList.add("none");
+//   }
+// });
